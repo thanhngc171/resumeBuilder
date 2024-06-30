68eb968613358987b4559ac0ef92ddad80fb2a02
Thanh Nguyen <nct@netcompany.com>
2024-06-30 15:16:28 +0700

    build: first init of resume builder

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+
+export const a = () => {
+  console.log('asd');
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { a } from '$lib/index';
+	console.log('asd');
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
